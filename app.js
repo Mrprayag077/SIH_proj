3dfd003934adf541a0b08612044caf955074cf5b
PRAYAG PRAMOD BHOSALE <99162236+Mrprayag077@users.noreply.github.com>
2022-07-29 17:56:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ mongoose.connect("mongodb+srv://prayag_SIHH:pp1234@cluster0.tuna9.mongodb.net/tu
  useUnifiedTopology: true });
 
 
-User.find({"reg_name": "kothrud"},function(err,users) {
-    if (err) console.warn(err);
-    console.warn(users);
-});
-
 app.use(bodyParser.urlencoded ({extended: true}));
 
 app.get("/", function (req, res){
@@ -23,11 +18,16 @@ app.get("/", function (req, res){
 
 app.post("/", function (req, res){
 //  res.send("jfffkmek");
-  
-await var n112  = String(req.body.n11);
-// var n222 = String(req.body.n22);
+var n112  = String(req.body.n11);
+var n222 = String(req.body.n22);
 
 res.send("the sum is"+`<br/><br />email:${n112}<br />password:${n222}<br />`);
+
+User.find({"reg_name": n112},function(err,users) {
+    if (err) console.warn(err);
+    console.warn(users);
+});
+
 });
 
 
@@ -35,4 +35,3 @@ app.listen(3000, function (req, res) {
    console.log("3000 port running........ ");
 });
 
- 
